30637ab2828c1304c8d2b245cdcb6eb5d88052b3
Igor Potemkin <ipotemkin@rambler.ru>
2022-09-12 13:24:42 +0300

    refactor: removed some unnecessary code

--- src/db/database.js
+++ src/db/database.js
@@ -1,12 +1,6 @@
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const process = require('process');
 
-// dotenv.config();
-// const { 
-//   PORT = 3005,
-//   MONGODB_URI
-// } = process.env;
 const getDb = (dbURI) => {
     mongoose.connect(dbURI,
         { useNewUrlParser: true, },
